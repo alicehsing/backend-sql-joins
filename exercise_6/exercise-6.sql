3feb105084e45147a908fea618039ba0122201bb
Alice Hsing <alicehsing@gmail.com>
2022-03-21 15:14:14 -0700

    wrote JOINS for exercise 6

--- exercise_6/exercise-6.sql
+++ exercise_6/exercise-6.sql
@@ -1,2 +1,20 @@
 -- find all film titles rented out by customers with the first_name Roberta
 -- Similar to exercise 5
+SELECT
+title
+FROM 
+inventory
+LEFT JOIN
+film
+ON
+inventory.film_id = film.film_id
+RIGHT JOIN
+rental
+ON
+inventory.inventory_id = rental.inventory_id
+RIGHT JOIN
+customer
+ON
+customer.customer_id = rental.customer_id
+WHERE customer.first_name = 'Roberta'
+
